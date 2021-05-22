a6e3946f4da03dfbf9626f1bcb5c0eb1e04f3baa
Roger Yeh <ryeh@432grandoak.me>
2021-05-22 15:26:59 -0700

    update

--- index.js
+++ index.js
@@ -1,101 +1,173 @@
 const puppeteer = require("puppeteer");
+
 const fs = require("fs");
 const diskdb = require("diskdb");
+const stringHash = require("string-hash");
 
-// class SiteScraper {
-//     _configHandler = {};
+class FsLayer {
+    constructor(dirName) {
+        this.dirName = dirName;
+        if (!fs.existsSync(this.dirName))
+            fs.mkdirSync(this.dirName);
+    }
+    readContent(file) {
+        const filePath = `${this.dirName}/${file}`;
+        if (fs.existsSync(filePath))
+            return fs.readFileSync(filePath, { encoding: 'utf8'});
+        return false;
+    }
 
-//     _getter(name, preferStale=true) {
-//         let staleValue = this._staleConfig[name];
-//         if (preferStale && staleValue)
-//             return staleValue;
+    writeContent(subDir, id, content) {
+        const dirPath = `${this.dirName}/${subDir}`;
+        if (!fs.existsSync(dirPath))
+            fs.mkdirSync(dirPath, { recursive: true });
 
-//         const newValue = this._configHandler[stale].apply();
-//         if (typeof newValue !== "undefined") {
-//             return newValue;
-//         }
-//     }
+        const filePath = `${dirPath}/${id}`;
+        fs.writeFileSync(filePath, content, { encoding: 'utf8' })
+    }
+}
 
-//     _getMetaFile() {
-//         return `./${this.siteConfig.siteId}.json`;
-//     }
+class SiteScraper {
+    _configHandler = {};
 
-//     constructor(browser, siteConfig) {
-//         this.browser = browser;
-//         this.page = await browser.newPage;
-//         this.siteConfig = siteConfig;
+    _getter(name, preferStale=true) {
+        let staleValue = this._staleConfig[name];
+        if (preferStale && staleValue)
+            return staleValue;
 
-//         this.metaData = JSON.parse(fs.readFileSync(this._getMetaFile()));
-
-//         await this.page.goto(url);
-//     }
-
-//     destructor() {
-//         fs.writeFileSync(dataFile, JSON.stringify(this._getMetaFile()));
-//     }
-// }
-
-const cool18Config = (() => {
-    const CollPageUrl = `https://www.cool18.com/bbs4/index.php?app=forum&act=gold&p=`;
-    return {
-        siteId: 'cool18',
-        url: 'https://www.cool18.com/bbs4/index.php',
-        configHandler: {
-            "numColPage": async () => {
-                return 18;
-            }
-        },
-        pages: {
-            getStoryListOnColPage: (ctx, pageId) => {
-                ctx.page.goTo(CollPageUrl + pageId);
-                
-            },
-            
+        const newValue = this._configHandler[stale].apply();
+        if (typeof newValue !== "undefined") {
+            return newValue;
         }
     }
-})();
+
+    _getMetaFile() {
+        return `./${this.siteConfig.siteId}.json`;
+    }
+
+    constructor(page, siteConfig) {
+        this.page = page;
+        this.siteConfig = siteConfig;
+
+        this.fragmentFs = new FsLayer(`_${siteConfig.siteId}_fragment`);
+
+        // this.metaData = JSON.parse(fs.readFileSync(this._getMetaFile()));
+    }
+
+    async scrape(type, ...parameters) {
+        const entity = this.siteConfig.entities[type];
+        if (!entity) throw `${type} is not a valid entity`;
+        const itemUrl = entity.url.call(null, this.siteConfig.baseUrl, ...parameters);
+
+        const res = await this.page.goto(itemUrl);
+        console.log(`${itemUrl} cached ${res.fromCache()}`)
+
+        const ret = await this.page.evaluate((entity) => {
+            var ret = {};
+            if (entity.parseRules) {
+                for (const k in entity.parseRules) {
+                    const [selector, ...processors] = entity.parseRules[k];
+                    const nodeList = document.querySelectorAll(selector);
+                    let value = nodeList;
+                    processors.forEach((processor) => {
+                        if (!value) return;
+                        switch (processor) {
+                            case 'single':
+                                value = value[0];
+                                break;
+                            case 'text':
+                                value = value.innerText;
+                                break;
+                            case "trim":
+                                value = value.trim();
+                                break;
+                        }
+                    });
+                    ret[k] = value;
+                }
+            }
+
+            if (entity.customParse) {
+                entity.customParse(ret);
+            }
+            return ret;
+        }, entity);
+        console.log(ret);
+        return ret;
+    }
+
+    destructor() {
+        fs.writeFileSync(dataFile, JSON.stringify(this._getMetaFile()));
+    }
+}
 
 const dcrConfig = (() => {
-    const CollPageUrl = `https://www.cool18.com/bbs4/index.php?app=forum&act=gold&p=`;
     return {
         siteId: 'dcr20',
-        url: 'https://www.cool18.com/bbs4/index.php',
-        url: {
+        baseUrl: 'https://www.20dcr.com',
+        entities: {
+            book: {
+                url: (baseUrl, id) => {
+                    return `${baseUrl}/book/${id}/`
+                },
+                customParse: (ret) => {
+                },
 
-            book: (name) => {
-                return `https://www.20dcr.com/book/${name}/`
-            },
+                parseRules: {
+                    "title": [".media-body > h1.book-name > a", "single", "text"],
+                    "description": [".row .book-detail", "single", "text", "trim"],
 
+                },
+            }
         },
-        method: {
-            getStoryListOnColPage: (ctx, pageId) => {
-                ctx.page.goTo(CollPageUrl + pageId);
-                
-            },
-            
-        }
     }
 })();
 
-const bookPath = "./book";
-
 const main = async () => {
-    // const browser = await puppeteer.connect({
-    //     browserWSEndpoint: 'wss://chrome.browserless.io?--user-data-dir=/tmp/session-123',
-    // });
-    const browser = await puppeteer.launch();
+    const args = [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-infobars',
+        '--window-position=0,0',
+        '--ignore-certifcate-errors',
+        '--ignore-certifcate-errors-spki-list',
+        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+    ];
+    const browser = await puppeteer.launch({
+        args,
+        userDataDir: '/tmp/puppeteerCache',
+        ignoreHTTPSErrors: true,
+        headless: true,
+    });
+    const newPage = await browser.newPage();
 
-    const db = diskdb.connect("./db");
-    db.loadCollections(['dcr20']);
+    const scraper = new SiteScraper(newPage, dcrConfig);
+    const bookId = "tuilijingjichang";
+    const bookInfo = await scraper.scrape("book", bookId);
 
-    // const c = new SiteScraper(browser, cool18Config);
 
-    /*
-    readIndex
+
+    // const db = diskdb.connect("./db");
+    // db.loadCollections(['dcr20']);
+
+    // c.upsert(bookId, c);
     
-    */
+    // i.chapters.forEach((cI) => {
+    //     const f = c.scrapeFragment(cI.url));
+    //     fragments[f.id] = cI.url;
+    //     c.saveFragmentContent(f.id, f.textContent);
 
-    //c.destructor();
+    // });
+    // c.upsert(bookId, {
+    //     fragments
+    // });
+
+    // for
+    // readIndex
+    
+    // */
+
+    // //c.destructor();
 
     await browser.close();
 };
